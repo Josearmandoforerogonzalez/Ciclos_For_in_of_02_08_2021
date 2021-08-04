0b6cecf351cacd48b1f47d0dc40e1d8bdab83b62
Josearmandoforerogonzalez <joseforero0310@gmail.com>
2021-08-04 17:34:54 -0500

    for-in-of

---
+++ main.js
@@ -0,0 +1,27 @@
+//IN
+for(i in `jose`){
+    console.log(`${i}`)
+}
+//OF
+for (i of `jose`){
+    console.log(`${i}`)
+}
+
+
+
+
+
+
+// for(let i=0; i<=1000; i+=7){
+//     console.log(`ciclo ${i}`);
+    
+// }
+
+// Tabla de multiplicar hasta x numero
+// let g = parseInt(prompt(`Ingrese la cantidad de tablas a multiplicar`)); //Ultima tabla
+// for (let i = 0; i < g; i++) {
+//     console.log(`%cTabla de multiplicar ${i}`, "font-family:cursive;color:orange;");
+//     for (let x = 0; x < 10; x++) {
+//         console.log(`%c${i} X ${(x+1)} = ${i*(x+1)}`,"font-family:Arial;");
+//     }
+// }
